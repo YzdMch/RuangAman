108086ab2890fef5f3b2dae1b6fe58ff72693001
Sarah Amelia Rachma <24082010139@student.upnjatim.ac.id>
2026-04-14 16:31:56 +0700

    registrasi & login

--- app/src/main/java/com/ubermensch/ruangamanv10/AuthViewModel.kt
+++ app/src/main/java/com/ubermensch/ruangamanv10/AuthViewModel.kt
@@ -1,6 +1,24 @@
 package com.ubermensch.ruangamanv10
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+sealed class Result {
+    object Success : Result()
+    data class Error(val message: String) : Result()
+}
+
 class AuthViewModel : ViewModel() {
+
+    private val _loginResult = MutableLiveData<Result>()
+    val loginResult: LiveData<Result> = _loginResult
+
+    fun login(email: String, pass: String) {
+        if (email.isNotEmpty() && pass.length >= 6) {
+            _loginResult.value = Result.Success
+        } else {
+            _loginResult.value = Result.Error("Email atau Password salah!")
+        }
+    }
 }
